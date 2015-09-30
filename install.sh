effd2caad873bb3fe2d247994f2cf7b44a45cb32
oscarmarcelo95 <oscarmarcelo95@gmail.com>
2015-09-30 14:41:31 -0500

    adds installation

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-git clone https://github.com/oelizondo/Sofia.git ~/.sofia
+hash git >/dev/null && /usr/bin/env git clone https://github.com/oelizondo/Sofia.git ~/.sofia || {
+  echo "git not installed"
+  exit
+}
 cp ~/.sofia/sofia /usr/local/bin/
 echo 'Sofia is installed! <3'
