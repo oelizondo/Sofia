51a5523f58283434683d65ea64e2703045de2d70
oscarmarcelo95 <oscarmarcelo95@gmail.com>
2015-09-30 20:15:08 -0500

    adds bash

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/oelizondo/Sofia.git ~/.sofia
+echo 'export PATH="$HOME/.sofia:$PATH"' >> ~/.bashrc
 echo 'export PATH="$HOME/.sofia:$PATH"' >> ~/.zshrc
 echo 'Sofia is installed! <3'
