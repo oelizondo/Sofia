0509b646d0b76523efa5fda800ce92749da92448
Oscar Elizondo <oscarmarcelo95@gmail.com>
2015-09-30 22:10:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 git clone https://github.com/oelizondo/Sofia.git ~/.sofia
-echo 'export PATH="$HOME/.sofia:$PATH"' >> ~/.bashrc
 echo 'export PATH="$HOME/.sofia:$PATH"' >> ~/.zshrc
 echo 'Sofia is installed! <3'
