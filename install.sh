45e4aa70e4bcbee077f18d8ec2f48d52bb0ea167
oscarmarcelo95 <oscarmarcelo95@gmail.com>
2015-10-01 12:59:53 -0500

    adds bash profile

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ git clone https://github.com/oelizondo/Sofia.git ~/.sofia
 sofia=$(echo $PATH | grep sofia)
 
 if [ -z $sofia ]; then
-    echo 'Adding Sofia to your path'
+    echo 'Adding Sofia to your path...'
+    echo 'export PATH="$HOME/.sofia:$PATH"' >> ~/.bashrc
     echo 'export PATH="$HOME/.sofia:$PATH"' >> ~/.zshrc
 fi
 
 echo 'Sofia is installed! <3'
+echo 'Restart your terminal to get working!'
